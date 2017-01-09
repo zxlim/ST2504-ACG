9136a30feced16c473d5a3ba41d62a408744fdb8
Lim Zhao Xiang <zxlim920@gmail.com>
2017-01-09 12:35:28 +0800

    Added JCE Test Program

---
+++ SourceCode/JCE_Test.java
@@ -0,0 +1,13 @@
+import javax.crypto.Cipher;
+
+public class JCE_Test {
+  public static void main(String[] args) {
+    try {
+      System.out.println("Hello World!");
+      int maxKeyLen = Cipher.getMaxAllowedKeyLength("AES");
+      System.out.println(maxKeyLen);
+    } catch (Exception e){
+      System.out.println("Sad world :(");
+    }
+  }
+}
