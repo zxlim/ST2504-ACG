7e9dfaf035b9d249a6f01a8a91baa70cf20dfe68
Lim Zhao Xiang <zxlim920@gmail.com>
2017-01-09 12:47:27 +0800

    Updated JCE Tester Program.

--- SourceCode/JCE_Test.java
+++ SourceCode/JCE_Test.java
@@ -3,11 +3,20 @@ import javax.crypto.Cipher;
 public class JCE_Test {
   public static void main(String[] args) {
     try {
-      System.out.println("Hello World!");
+      System.out.println("Checking maximum AES Key Length...");
+      Thread.sleep(2000);
       int maxKeyLen = Cipher.getMaxAllowedKeyLength("AES");
-      System.out.println(maxKeyLen);
-    } catch (Exception e){
-      System.out.println("Sad world :(");
+      System.out.println("Maximum keylength for AES ciphers: " + maxKeyLen);
+      if (maxKeyLen <= 128) {
+            System.out.println("Java Cryptographic Extension (JCE) not installed or corrupted.");
+            System.out.println("Download JCE for Java 8 at: http://bit.ly/1RCPSqx");
+      } else {
+            System.out.println("Java Cryptographic Extension installed successfully!");
+      }
+    } catch (Exception ex){
+      System.out.println("Ohmygawd an error occured !!!\n");
+      System.out.println(ex);
+      ex.printStackTrace();
     }
   }
 }
