b1c4c668259cad1dd47c33ad1814a2242387663a
tukiman <aiman.abdulrashid.aar@gmail.com>
2017-02-15 13:32:08 +0800

    Test.java for keystore retrival

--- SourceCode [Gerald&Aiman]/test.java
+++ SourceCode [Gerald&Aiman]/test.java
@@ -7,26 +7,44 @@ import java.security.PublicKey;
 import java.security.cert.Certificate;
 
 public class test {
-  public static void main(String[] argv) throws Exception {
-    FileInputStream is = new FileInputStream("ServerKeyStore");
+    public static void main(String[] argv) throws Exception {
 
-    KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-    keystore.load(is, "1qwer$#@!".toCharArray());
+        //getting KeyStore Ready
+        FileInputStream is = new FileInputStream("ServerKeyStore");
 
-    String alias = "server";
+        KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+        keystore.load(is, "1qwer$#@!".toCharArray());
 
-    Key key = keystore.getKey(alias, "1qwer$#@!".toCharArray());
-    if (key instanceof PrivateKey) {
-      // Get certificate of public key
-      Certificate cert = keystore.getCertificate(alias);
+        String alias = "server";
 
-      // Get public key
-      PublicKey publicKey = cert.getPublicKey();
+        Key key = keystore.getKey(alias, "1qwer$#@!".toCharArray());
 
-      System.out.println(publicKey);
+        if (key instanceof PrivateKey) {
+            // Get certificate of public key
+            Certificate cert = keystore.getCertificate(alias);
 
-      // Return a key pair
-      //new KeyPair(publicKey, (PrivateKey) key);
+            // Get public key
+            PublicKey publicKey = cert.getPublicKey();
+
+            //Get private key
+            PrivateKey privateKey = (PrivateKey)keystore.getKey(alias, "1qwer$#@!".toCharArray());
+
+            byte[] text = Crypto.strToBytes("hello");
+            //encrypt using public RSA
+            byte[] ciphertext = Crypto.encrypt_RSA(text, publicKey);
+
+            System.out.println(ciphertext);
+
+            //decrypt using public RSA
+            byte[] deciphertext = Crypto.decrypt_RSA(ciphertext, privateKey);
+            String deciphered = Crypto.bytesToStr(deciphertext);
+
+            System.out.println("\n"+ deciphered);
+
+            // Return a key pair
+            //KeyPair keyPair = new KeyPair(publicKey, (PrivateKey) key);
+            //PrivateKey privateKey = keyPair.getPrivate();
+
+        }
     }
-  }
 }
