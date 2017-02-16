2811bba13f283e1d28550b51ee5468a948c0631d
tukiman <aiman.abdulrashid.aar@gmail.com>
2017-02-16 15:32:28 +0800

    For Gerald WTF

--- Aiman&Gerald/RegisterServer.java
+++ Aiman&Gerald/RegisterServer.java
@@ -1,62 +1,87 @@
 import java.io.FileInputStream;
-import java.security.Key;
-import java.security.KeyStore;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.cert.Certificate;
 
 import java.io.*;
+import java.net.*;
+
 
 public class RegisterServer{
+
+    private static Socket socket;
+    private static ObjectInputStream sInput;
+    private static ObjectOutputStream sOutput;
+
     public static void main(String[] args) throws Exception {
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(1449);
+            System.out.println("Listening on port: 1449");
+
+            //Accept client connection
+            boolean go = true;
+            PKI serverRSA = Crypto.ksPrivateKey("Server.keystore", "serverrsa");
+
+
+            while(go){
+                //Accept client connection
+                socket = serverSocket.accept();
+                String clientIp = socket.getLocalSocketAddress().toString();
+                System.out.println("Client connected" + clientIp );
+
+                //Create new I/O stream from socket
+
+                sInput = new ObjectInputStream(socket.getInputStream());
+                //sOutput =  new ObjectOutputStream(socket.getOutputStream());
+
+                //sOutput.writeObject("Success!");
+                System.out.println("Input initialized");
+
+
+
+                //Read input from Client
+                Credentials newUser = (Credentials) sInput.readObject();
+
+                String name = Crypto.bytesToStr(Crypto.decrypt_RSA(newUser.getUsername(), serverRSA.getPrivate()));
+                System.out.println("Message from Client: " + name);
+                sInput.close();
+                sOutput.close();
+
+            }
+
+            socket.close();
+        } catch (Exception e) {
+            System.out.print(e);
+            e.printStackTrace();
+            return;
+        }
+
         /*
-        user = Credentials;
+        Credentials newUser = (Credentials) sInput.readObject();
         byte[] username = user.getUsername();
         byte[] password = user.getPassword();
-        byte[] RSApub = user.getRSApub();
-        byte[] ECDSApub = user.getECDSApub;
         */
-        PublicKey publicKey = null;
-        PrivateKey privateKey = null;
-
         //getting KeyStore Ready
-        FileInputStream is = new FileInputStream("ServerKeyStore");
-        KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
-        keystore.load(is, "1qwer$#@!".toCharArray());
-        String alias = "serverrsa";
-        Key key = keystore.getKey(alias, "1qwer$#@!".toCharArray());
-
-        if (key instanceof PrivateKey) {
-            // Get certificate of public key
-            Certificate cert = keystore.getCertificate(alias);
-
-            // Get public key
-            publicKey = cert.getPublicKey();
-
-            //Get private key
-            privateKey = (PrivateKey)keystore.getKey(alias, "1qwer$#@!".toCharArray());
-
-        }
+        //PKI serverRSApub = Crypto.ksPublicKey("Server.keystore", "serverrsa");
 
         //Encryption
-        byte[] username = Crypto.encrypt_RSA(Crypto.strToBytes("jasong"), publicKey);
-        byte[] password = Crypto.encrypt_RSA(Crypto.strToBytes("1qwe#@!"), publicKey);
-        //Name Decryption
-        String name = Crypto.bytesToStr(Crypto.decrypt_RSA(username, privateKey));
-        String passwd = Crypto.bytesToStr(Crypto.decrypt_RSA(password, privateKey));
-        //System.out.println(passwd);
+        // byte[] username = Crypto.encrypt_RSA(Crypto.strToBytes("aiman"), serverRSApub.getPublic());
+        // byte[] password = Crypto.encrypt_RSA(Crypto.strToBytes("1qwe#@!"), serverRSApub.getPublic());
+        // //Name Decryption
+        // String name = Crypto.bytesToStr(Crypto.decrypt_RSA(username, serverRSA.getPrivate()));
+        // String passwd = Crypto.bytesToStr(Crypto.decrypt_RSA(password, serverRSA.getPrivate()));
 
-        if (file.validateName(name) == 1) {
-            System.out.println("Invalid Username");
-        } else {
 
-            byte[] salt = Crypto.secureRand(32);
-            byte[] hashedpw = Crypto.pbkdf2(passwd, salt);
-            String strHashed = Crypto.bytesToBase64(hashedpw);
-            String strSalt = Crypto.bytesToBase64(salt);
-            file.credentialWriter(name, strHashed , strSalt);
-            System.out.println("Username Valid");
-        }
+        // if (file.validateName(name) == 1) {
+        //     System.out.println("Invalid Username");
+        // } else {
+        //
+        //     byte[] salt = Crypto.secureRand(32);
+        //     byte[] hashedpw = Crypto.pbkdf2(passwd, salt);
+        //     String strHashed = Crypto.bytesToBase64(hashedpw);
+        //     String strSalt = Crypto.bytesToBase64(salt);
+        //     file.credentialWriter(name, strHashed , strSalt);
+        //     System.out.println("Username Valid");
+        //
+        // }
 
     }
 }
--- Aiman&Gerald/file.java
+++ Aiman&Gerald/file.java
@@ -2,37 +2,29 @@ import java.io.*;
 
 public class file {
     public static void credentialWriter(String name, String password, String salt) throws Exception {
-        File file = new File("file.txt");
 
         // creates a FileWriter Object
-        FileWriter writer = new FileWriter(file, true);
+        FileWriter writer = new FileWriter("passwd", true);
 
         // Writes the content to the file
         writer.write(name + ":" + password + ":" + salt + System.getProperty( "line.separator" ));
         writer.flush();
         writer.close();
 
-        // Creates a FileReader Object
-        /*FileReader fr = new FileReader(file);
-        char [] a = new char[50];
-        fr.read(a);   // reads the content to the array
-
-        for(char c : a)
-        System.out.print(c);   // prints the characters one by one
-        fr.close();*/
     }
 
     public static int validateName(String name) throws Exception {
 
 
-        FileReader file = new FileReader(new File("file.txt"));
+        final InputStreamReader file = new InputStreamReader(new FileInputStream("passwd"), "UTF-8");
         BufferedReader read = new BufferedReader(file);
 
-        String line = read.readLine();
-        String firstWord = null;
+        String line;
 
         while ((line = read.readLine()) != null) {
-            firstWord= line.substring(0, line.indexOf(":"));
+            String[] word = line.split(":");
+            String firstWord = word[0];
+
             if (name.equals(firstWord)) {
                 return 1;
             }
